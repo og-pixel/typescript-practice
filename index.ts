fbbd96da9f72da3cb46452c7ec1c8a16df4c6f91
Milosz Jakubanis <milosz.jakubanis@lockular.com>
2021-12-13 15:40:09 +0000

    wip

--- index.ts
+++ index.ts
@@ -1,14 +1,24 @@
 interface Vehicle {
   name: string
+  age: number
 }
 
 class Car implements Vehicle {
 
-  constructor(name: String){
-    name = name
-  }
   name: string;
+  age: number;
+
+  constructor(name: string){
+    this.name = name;
+    this.age = 22;
+  }
+
+  toString(): string {
+    return this.name;
+  }
 }
 
+var car = new Car("mazda")
 
-console.log(new Car("hello"))
+console.log(car.toString())
+console.log(car.name, car.age)
